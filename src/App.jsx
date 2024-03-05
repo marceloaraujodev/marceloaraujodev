7bd98926a79950903693f82f69973b6bacb59ba1
codetrsh <134286416+codetrsh@users.noreply.github.com>
2024-03-05 13:28:08 -0300

    sh

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
         <main>
           <Home />
           <BlogProject />
-          {/* <ShopCartProject /> */}
+          <ShopCartProject />
           {/* <FipeProject /> */}
           {/* <About /> */}
         </main>
